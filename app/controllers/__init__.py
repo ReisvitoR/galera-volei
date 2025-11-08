1ce7eb573cabd6bd5e826d48b4a174f4eab93f21
ReisvitoR <reishuvitor@gmail.com>
2025-11-08 18:55:47 -0300

    ajust-convite-cors

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,7 +1,8 @@
-from app.controllers import auth_controller, usuario_controller, partida_controller
+from app.controllers import auth_controller, usuario_controller, partida_controller, convite_controller
 
 __all__ = [
     "auth_controller",
     "usuario_controller", 
-    "partida_controller"
+    "partida_controller",
+    "convite_controller"
 ]
