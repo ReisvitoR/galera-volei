a5bd512d025330418aedb44c02df4ea3287bae3f
ReisvitoR <reishuvitor@gmail.com>
2025-11-10 19:37:14 -0300

    att-db

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     ALLOWED_HOSTS: List[str] = Field(default=["*"])
     
     # Database
-    #DATABASE_URL: str = "postgresql://koyeb-adm:npg_h9oeRMuWa3Li@ep-broad-rice-a2qzyo05.eu-central-1.pg.koyeb.app/koyebdb"
+    DATABASE_URL: str = "sqlite:///./galera_volei.db"
     
     # Security
     SECRET_KEY: str = "sua-chave-secreta-super-forte-aqui"
